133e00d3577f05dacd4c6573e33066e3f615d948
Filip Krawiec <Filip.Krawiec@gmail.com>
2021-06-16 11:26:42 +0200

    removed unnecessary annotations and added logger

--- src/main/java/com/krawart/lombok/shared/presentation/v1/controllers/ExceptionHandlerController.java
+++ src/main/java/com/krawart/lombok/shared/presentation/v1/controllers/ExceptionHandlerController.java
@@ -1,17 +1,23 @@
 package com.krawart.lombok.shared.presentation.v1.controllers;
 
-import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Arrays;
+
+@Slf4j
 @ControllerAdvice
-@NoArgsConstructor
 public class ExceptionHandlerController {
 
     @ExceptionHandler
     protected ResponseEntity<String> handleGeneralError(RuntimeException ex) {
-        String bodyOfResponse = "Oops. Some error occured. Please contact our support";
+        log.error("Some unhandled error occurred. " +
+                "message: " + ex.getMessage() +
+                ", stack: " + Arrays.toString(ex.getStackTrace()));
+
+        String bodyOfResponse = "Oops. Some error occurred. Please contact our support";
         return ResponseEntity.internalServerError().body(bodyOfResponse);
     }
 }
